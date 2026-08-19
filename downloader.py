76ac65e275d97081e3bd689f4b37f3dd250795af
iranVer <sosis2283@gmail.com>
2026-08-20 03:18:07 +0330

    Implement video and audio download functions
    Added functions to download video and audio from URLs using yt-dlp.

--- downloader.py
+++ downloader.py
@@ -1,1 +1,47 @@
+import os
+import tempfile
+import yt_dlp
 
+
+def download_video(url):
+    folder = tempfile.mkdtemp()
+
+    output = os.path.join(
+        folder,
+        "%(title)s.%(ext)s"
+    )
+
+    options = {
+        "format": "best[ext=mp4]/best",
+        "outtmpl": output,
+        "noplaylist": True,
+        "quiet": True,
+    }
+
+    with yt_dlp.YoutubeDL(options) as ydl:
+        info = ydl.extract_info(url, download=True)
+        filename = ydl.prepare_filename(info)
+
+    return filename
+
+
+def download_audio(url):
+    folder = tempfile.mkdtemp()
+
+    output = os.path.join(
+        folder,
+        "%(title)s.%(ext)s"
+    )
+
+    options = {
+        "format": "bestaudio/best",
+        "outtmpl": output,
+        "noplaylist": True,
+        "quiet": True,
+    }
+
+    with yt_dlp.YoutubeDL(options) as ydl:
+        info = ydl.extract_info(url, download=True)
+        filename = ydl.prepare_filename(info)
+
+    return filename
